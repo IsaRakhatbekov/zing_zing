91570d2c21bfff3d1b64ae2e4031d534ae6bed2b
Isa <isarahatbekov250@gmail.com>
2025-10-25 23:18:02 +0600

    some

--- src/app/news/page.tsx
+++ src/app/news/page.tsx
@@ -1,11 +1,9 @@
 import Image from 'next/image'
 import styles from './page.module.scss'
 
-import eventGirls from '@/assets/images/news-Events-model.png'
 import newHeroModel from '@/assets/images/news-hero-model.png'
 import newsHeroLeft from '@/assets/images/news-hero-packet.png'
 import newsPaper from '@/assets/images/news-paper.png'
-import newsTshirts from '@/assets/images/news-tshirts.png'
 import Button from '@/components/ui/Button'
 const page = () => {
 	// News page
@@ -61,40 +59,6 @@ const page = () => {
 					</div>
 				</div>
 			</section>
-
-			<section className={styles.events}>
-				<div className={`${styles.container} container`}>
-					<div className={styles.wrapper}>
-						<div className={styles.imgWrapper}>
-							<Image src={eventGirls} alt='#' />
-						</div>
-						<div className={styles.content}>
-							<h2 className={styles.title}>Events & Exhibitions</h2>
-							<p className={styles.text}>
-								ZingZing loves to meet people!We take part in food fairs, expos,
-								and local festivals — sharing our story, our spice, and our
-								smiles with everyone we meet.
-							</p>
-							<Button text='Upcoming Events' className='purple' href='#' />
-						</div>
-					</div>
-				</div>
-			</section>
-
-			<div className={styles.purpleWrapper}>
-				<div className={styles.content}>
-					<h2 className={styles.title}>Stewardship</h2>
-					<p className={styles.text}>
-						We care for taste and for the planet. ZingZing continuously improves
-						its packaging efficiency, reduces waste, and seeks recyclable
-						materials wherever possible.
-					</p>
-					<Button text='Contact Us' className='pink' href='#' />
-				</div>
-				<div className={styles.imgWrapper}>
-					<Image src={newsTshirts} alt='' />
-				</div>
-			</div>
 		</>
 	)
 }
