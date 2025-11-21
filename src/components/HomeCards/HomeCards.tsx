805f93a38c31f79b1920cdf76464e7746aa28e69
Isa <isarahatbekov250@gmail.com>
2025-11-21 15:22:00 +0600

    some

--- src/components/HomeCards/HomeCards.tsx
+++ src/components/HomeCards/HomeCards.tsx
@@ -27,50 +27,9 @@ export default function HomeCards({ thirdSection }: HomeCardsProps) {
 		<section className={styles.HomeCards} key={forceUpdate}>
 			{/* Блок для английского языка */}
 			{currentLang === 'ENG' && (
-				<div className={`${styles.container} ${styles.containerENG} container`}>
-					{/* <div className={styles.wrapperTop}>
-						<div className={styles.imgWrapper}>
-							<Image
-								src={thirdSection.cards[0].image}
-								alt='#'
-								width={100}
-								height={100}
-							/>
-						</div>
-						<div className={styles.imgWrapper}>
-							<Image
-								src={thirdSection.cards[1].image}
-								alt='#'
-								width={100}
-								height={100}
-							/>
-						</div>
-						<div className={styles.imgWrapper}>
-							<Image
-								src={thirdSection.cards[2].image}
-								alt='#'
-								width={100}
-								height={100}
-							/>
-						</div>
-					</div>
-					<div className={styles.bottom}>
-						<div className={styles.contentWrapper}>
-							<p>{thirdSection.cards[0].text}</p>
-							<Link href={'/products'} className={styles.button}>
-								Learn More
-							</Link>
-						</div>
-						<div className={styles.contentWrapper}>
-							<p>{thirdSection.cards[1].text}</p>
-							<Button className='purple' href='/products' text='Learn More' />
-						</div>
-						<div className={styles.contentWrapper}>
-							<p>{thirdSection.cards[2].text}</p>
-							<Button className='orange' href='/products' text='Learn More' />
-						</div>
-					</div> */}
-				</div>
+				<div
+					className={`${styles.container} ${styles.containerENG} container`}
+				></div>
 			)}
 
 			{/* Блок для русского языка */}
@@ -123,50 +82,23 @@ export default function HomeCards({ thirdSection }: HomeCardsProps) {
 
 			{/* Блок для казахского языка */}
 			{currentLang === 'KAZ' && (
-				<div className={`${styles.container} ${styles.containerKAZ} container`}>
-					{/* <div className={styles.wrapperTop}>
-						<div className={styles.imgWrapper}>
-							<Image
-								src={thirdSection.cards[0].image}
-								alt='#'
-								width={100}
-								height={100}
-							/>
-						</div>
-						<div className={styles.imgWrapper}>
-							<Image
-								src={thirdSection.cards[1].image}
-								alt='#'
-								width={100}
-								height={100}
-							/>
-						</div>
-						<div className={styles.imgWrapper}>
-							<Image
-								src={thirdSection.cards[2].image}
-								alt='#'
-								width={100}
-								height={100}
-							/>
-						</div>
-					</div>
-					<div className={styles.bottom}>
-						<div className={styles.contentWrapper}>
-							<p>{thirdSection.cards[0].text}</p>
-							<Link href={'/products'} className={styles.button}>
-								Learn More
-							</Link>
-						</div>
-						<div className={styles.contentWrapper}>
-							<p>{thirdSection.cards[1].text}</p>
-							<Button className='purple' href='/products' text='Learn More' />
-						</div>
-						<div className={styles.contentWrapper}>
-							<p>{thirdSection.cards[2].text}</p>
-							<Button className='orange' href='/products' text='Learn More' />
-						</div>
-					</div> */}
-				</div>
+				<div
+					className={`${styles.container} ${styles.containerKAZ} container`}
+				></div>
+			)}
+
+			{/* Блок для узбекского языка */}
+			{currentLang === 'UZB' && (
+				<div
+					className={`${styles.container} ${styles.containerUZB} container`}
+				></div>
+			)}
+
+			{/* Блок для турецкого языка */}
+			{currentLang === 'TUR' && (
+				<div
+					className={`${styles.container} ${styles.containerTUR} container`}
+				></div>
 			)}
 		</section>
 	)
