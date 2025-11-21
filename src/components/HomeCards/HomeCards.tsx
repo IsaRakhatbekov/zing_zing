e8a5ec44ee0684bc9f6190d6bf9a1a54a3024925
Isa <isarahatbekov250@gmail.com>
2025-11-21 17:56:10 +0600

    need to finish links and product cards adaptive

--- src/components/HomeCards/HomeCards.tsx
+++ src/components/HomeCards/HomeCards.tsx
@@ -1,4 +1,5 @@
 'use client'
+
 import { useLanguage } from '@/context/LanguageContext'
 import { useEffect, useState } from 'react'
 import styles from './HomeCards.module.scss'
@@ -14,6 +15,39 @@ interface HomeCardsProps {
 	thirdSection: ThirdSectionProps
 }
 
+// Локальные переводы для кнопок
+const buttonTranslations = {
+	learnMore: {
+		ENG: 'Learn More',
+		RUS: 'Узнать больше',
+		KAZ: 'Көбірек Білу',
+		UZB: 'Batafsil',
+		TUR: 'Daha Fazla',
+	},
+}
+
+// Локальный компонент кнопки
+interface LocalButtonProps {
+	translationKey: keyof typeof buttonTranslations
+	href?: string
+	className: string
+}
+
+const LocalButton = ({
+	translationKey,
+	href = '/products',
+	className,
+}: LocalButtonProps) => {
+	const { currentLang } = useLanguage()
+	const buttonText = buttonTranslations[translationKey][currentLang]
+
+	return (
+		<a href={href} className={`${styles.button} ${styles[className]}`}>
+			{buttonText}
+		</a>
+	)
+}
+
 export default function HomeCards({ thirdSection }: HomeCardsProps) {
 	const { currentLang } = useLanguage()
 	const [forceUpdate, setForceUpdate] = useState(0)
@@ -25,80 +59,74 @@ export default function HomeCards({ thirdSection }: HomeCardsProps) {
 
 	return (
 		<section className={styles.HomeCards} key={forceUpdate}>
-			{/* Блок для английского языка */}
+			{/* ENG */}
 			{currentLang === 'ENG' && (
-				<div
-					className={`${styles.container} ${styles.containerENG} container`}
-				></div>
-			)}
-
-			{/* Блок для русского языка */}
-			{currentLang === 'RUS' && (
-				<div className={`${styles.container} ${styles.containerRUS} container`}>
-					{/* <div className={styles.wrapperTop}>
-						<div className={styles.imgWrapper}>
-							<Image
-								src={thirdSection.cards[0].image}
-								alt='#'
-								width={100}
-								height={100}
-							/>
-						</div>
-						<div className={styles.imgWrapper}>
-							<Image
-								src={thirdSection.cards[1].image}
-								alt='#'
-								width={100}
-								height={100}
-							/>
-						</div>
-						<div className={styles.imgWrapper}>
-							<Image
-								src={thirdSection.cards[2].image}
-								alt='#'
-								width={100}
-								height={100}
-							/>
-						</div>
+				<>
+					<div
+						className={`${styles.container} ${styles.containerENG} container`}
+					/>
+					<div className={styles.buttonsRow}>
+						<LocalButton translationKey='learnMore' className='pink' />
+						<LocalButton translationKey='learnMore' className='purple' />
+						<LocalButton translationKey='learnMore' className='orange' />
 					</div>
-					<div className={styles.bottom}>
-						<div className={styles.contentWrapper}>
-							<p>{thirdSection.cards[0].text}</p>
-							<Link href={'/products'} className={styles.button}>
-								Learn More
-							</Link>
-						</div>
-						<div className={styles.contentWrapper}>
-							<p>{thirdSection.cards[1].text}</p>
-							<Button className='purple' href='/products' text='Learn More' />
-						</div>
-						<div className={styles.contentWrapper}>
-							<p>{thirdSection.cards[2].text}</p>
-							<Button className='orange' href='/products' text='Learn More' />
-						</div>
-					</div> */}
-				</div>
+				</>
 			)}
 
-			{/* Блок для казахского языка */}
+			{/* RUS */}
+			{currentLang === 'RUS' && (
+				<>
+					<div
+						className={`${styles.container} ${styles.containerRUS} container`}
+					/>
+					<div className={styles.buttonsRow}>
+						<LocalButton translationKey='learnMore' className='pink' />
+						<LocalButton translationKey='learnMore' className='purple' />
+						<LocalButton translationKey='learnMore' className='orange' />
+					</div>
+				</>
+			)}
+
+			{/* KAZ */}
 			{currentLang === 'KAZ' && (
-				<div
-					className={`${styles.container} ${styles.containerKAZ} container`}
-				></div>
+				<>
+					<div
+						className={`${styles.container} ${styles.containerKAZ} container`}
+					/>
+					<div className={styles.buttonsRow}>
+						<LocalButton translationKey='learnMore' className='pink' />
+						<LocalButton translationKey='learnMore' className='purple' />
+						<LocalButton translationKey='learnMore' className='orange' />
+					</div>
+				</>
 			)}
 
-			{/* Блок для узбекского языка */}
+			{/* UZB */}
 			{currentLang === 'UZB' && (
-				<div
-					className={`${styles.container} ${styles.containerUZB} container`}
-				></div>
+				<>
+					<div
+						className={`${styles.container} ${styles.containerUZB} container`}
+					/>
+					<div className={styles.buttonsRow}>
+						<LocalButton translationKey='learnMore' className='pink' />
+						<LocalButton translationKey='learnMore' className='purple' />
+						<LocalButton translationKey='learnMore' className='orange' />
+					</div>
+				</>
 			)}
 
-			{/* Блок для турецкого языка */}
+			{/* TUR */}
 			{currentLang === 'TUR' && (
-				<div
-					className={`${styles.container} ${styles.containerTUR} container`}
-				></div>
+				<>
+					<div
+						className={`${styles.container} ${styles.containerTUR} container`}
+					/>
+					<div className={styles.buttonsRow}>
+						<LocalButton translationKey='learnMore' className='pink' />
+						<LocalButton translationKey='learnMore' className='purple' />
+						<LocalButton translationKey='learnMore' className='orange' />
+					</div>
+				</>
 			)}
 		</section>
 	)
