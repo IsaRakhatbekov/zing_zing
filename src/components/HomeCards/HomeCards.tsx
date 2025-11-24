aee080cdabc0a3a73e11221ed8e13c0efb594646
Isa <isarahatbekov250@gmail.com>
2025-11-24 11:19:24 +0600

    some

--- src/components/HomeCards/HomeCards.tsx
+++ src/components/HomeCards/HomeCards.tsx
@@ -59,75 +59,77 @@ export default function HomeCards({ thirdSection }: HomeCardsProps) {
 
 	return (
 		<section className={styles.HomeCards} key={forceUpdate}>
-			{/* ENG */}
-			{currentLang === 'ENG' && (
-				<>
-					<div
-						className={`${styles.container} ${styles.containerENG} container`}
-					/>
-					<div className={styles.buttonsRow}>
-						<LocalButton translationKey='learnMore' className='pink' />
-						<LocalButton translationKey='learnMore' className='purple' />
-						<LocalButton translationKey='learnMore' className='orange' />
-					</div>
-				</>
-			)}
+			<div className={`container`}>
+				{/* ENG */}
+				{currentLang === 'ENG' && (
+					<>
+						<div
+							className={`${styles.container} ${styles.containerENG} container`}
+						/>
+						<div className={styles.buttonsRow}>
+							<LocalButton translationKey='learnMore' className='pink' />
+							<LocalButton translationKey='learnMore' className='purple' />
+							<LocalButton translationKey='learnMore' className='orange' />
+						</div>
+					</>
+				)}
 
-			{/* RUS */}
-			{currentLang === 'RUS' && (
-				<>
-					<div
-						className={`${styles.container} ${styles.containerRUS} container`}
-					/>
-					<div className={styles.buttonsRow}>
-						<LocalButton translationKey='learnMore' className='pink' />
-						<LocalButton translationKey='learnMore' className='purple' />
-						<LocalButton translationKey='learnMore' className='orange' />
-					</div>
-				</>
-			)}
+				{/* RUS */}
+				{currentLang === 'RUS' && (
+					<>
+						<div
+							className={`${styles.container} ${styles.containerRUS} container`}
+						/>
+						<div className={styles.buttonsRow}>
+							<LocalButton translationKey='learnMore' className='pink' />
+							<LocalButton translationKey='learnMore' className='purple' />
+							<LocalButton translationKey='learnMore' className='orange' />
+						</div>
+					</>
+				)}
 
-			{/* KAZ */}
-			{currentLang === 'KAZ' && (
-				<>
-					<div
-						className={`${styles.container} ${styles.containerKAZ} container`}
-					/>
-					<div className={styles.buttonsRow}>
-						<LocalButton translationKey='learnMore' className='pink' />
-						<LocalButton translationKey='learnMore' className='purple' />
-						<LocalButton translationKey='learnMore' className='orange' />
-					</div>
-				</>
-			)}
+				{/* KAZ */}
+				{currentLang === 'KAZ' && (
+					<>
+						<div
+							className={`${styles.container} ${styles.containerKAZ} container`}
+						/>
+						<div className={styles.buttonsRow}>
+							<LocalButton translationKey='learnMore' className='pink' />
+							<LocalButton translationKey='learnMore' className='purple' />
+							<LocalButton translationKey='learnMore' className='orange' />
+						</div>
+					</>
+				)}
 
-			{/* UZB */}
-			{currentLang === 'UZB' && (
-				<>
-					<div
-						className={`${styles.container} ${styles.containerUZB} container`}
-					/>
-					<div className={styles.buttonsRow}>
-						<LocalButton translationKey='learnMore' className='pink' />
-						<LocalButton translationKey='learnMore' className='purple' />
-						<LocalButton translationKey='learnMore' className='orange' />
-					</div>
-				</>
-			)}
+				{/* UZB */}
+				{currentLang === 'UZB' && (
+					<>
+						<div
+							className={`${styles.container} ${styles.containerUZB} container`}
+						/>
+						<div className={styles.buttonsRow}>
+							<LocalButton translationKey='learnMore' className='pink' />
+							<LocalButton translationKey='learnMore' className='purple' />
+							<LocalButton translationKey='learnMore' className='orange' />
+						</div>
+					</>
+				)}
 
-			{/* TUR */}
-			{currentLang === 'TUR' && (
-				<>
-					<div
-						className={`${styles.container} ${styles.containerTUR} container`}
-					/>
-					<div className={styles.buttonsRow}>
-						<LocalButton translationKey='learnMore' className='pink' />
-						<LocalButton translationKey='learnMore' className='purple' />
-						<LocalButton translationKey='learnMore' className='orange' />
-					</div>
-				</>
-			)}
+				{/* TUR */}
+				{currentLang === 'TUR' && (
+					<>
+						<div
+							className={`${styles.container} ${styles.containerTUR} container`}
+						/>
+						<div className={styles.buttonsRow}>
+							<LocalButton translationKey='learnMore' className='pink' />
+							<LocalButton translationKey='learnMore' className='purple' />
+							<LocalButton translationKey='learnMore' className='orange' />
+						</div>
+					</>
+				)}
+			</div>
 		</section>
 	)
 }
